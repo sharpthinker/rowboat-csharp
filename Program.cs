0a320c901ab304c6934ef41c62c0f4af7edff193
sharpthinker <sharpthinker@noreply.github.com>
2023-09-27 07:58:46 -0400

    Init project and solve problem

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+IEnumerable<int> GetNeighborStates(int currentState) {
+    int[] mutations = {
+        // 0b1101,
+        // 0b1011,
+        // 0b1110,
+        0b1001,
+        0b1010,
+        0b1100,
+    };
+
+    for (int i = 0; i < mutations.Length; i++) {
+        yield return currentState ^ mutations[i];
+    }
+}
+
+int GetCount() {
+    const int startState = 0;
+    const int finalState = 15;
+
+    int[] counts = new int[16];
+    bool[] visited = new bool[16];
+    Array.Fill(counts, -1);
+    Array.Fill(visited, false);
+    counts[0] = 1;
+
+    int GetCountInner(int state) {
+        if (state == startState) return 1;
+        if (counts[state] >= 0) {
+            return counts[state];
+        }
+
+        int sum = 0;
+        IEnumerable<int> neighbors = GetNeighborStates(state);
+        
+        visited[state] = true;
+        foreach(int neighbor in neighbors) {
+            if (visited[neighbor]) continue;
+
+            sum += GetCountInner(neighbor);
+        }
+        counts[state] = sum;
+        visited[state] = false;
+
+        return sum;
+    }
+
+    return GetCountInner(finalState);
+}
+
+Console.WriteLine(GetCount());
